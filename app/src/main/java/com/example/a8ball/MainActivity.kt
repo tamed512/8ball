404df37a94b3b5d9f4b6269fc6b80c362aa107d8
ZSkull221 <zakel536@learner.dvusd.org>
2021-12-13 11:19:58 -0700

    Added More Balls

--- app/src/main/java/com/example/a8ball/MainActivity.kt
+++ app/src/main/java/com/example/a8ball/MainActivity.kt
@@ -12,26 +12,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
+
         //hello
         
-=======
+
 
         // link all the variables with its id
         val imageView = findViewById<View>(R.id.image_eightBall) as ImageView
         val button = findViewById<View>(R.id.askButton) as Button
 
         // create an array to store all the images
-        val a = intArrayOf(R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5)
+        val a = intArrayOf(R.drawable.ball1, R.drawable.ball2, R.drawable.ball3, R.drawable.ball4, R.drawable.ball5, R.drawable.ball)
 
         // ask button's onClick function
         button.setOnClickListener { // generate a number using Random() function
             val random = Random()
-            val x = random.nextInt(4)
+            val x = random.nextInt(6)
 
             // set the image to the view
             imageView.setImageResource(a[x])
         }
->>>>>>> main
+
     }
 }
